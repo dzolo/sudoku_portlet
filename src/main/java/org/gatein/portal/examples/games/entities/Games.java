14c433a2767c2401abd9c772829e5305714daf82
Ondřej Fibich <ondrej.fibich@gmail.com>
2012-02-20 20:42:45 +0100

    Fixes comments.

--- src/main/java/org/gatein/portal/examples/games/entities/Games.java
+++ src/main/java/org/gatein/portal/examples/games/entities/Games.java
@@ -163,7 +163,6 @@ public class Games implements Serializable
     @Override
     public boolean equals(Object object)
     {
-        // TODO: Warning - this method won't work in the case the id fields are not set
         if (!(object instanceof Games)) {
             return false;
         }
